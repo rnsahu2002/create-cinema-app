0a9955722a41f7f92bd27f297d2db6883531c038
rnsahu2002 <rnsahu2002@gmail.com>
2025-06-16 19:38:02 +0530

    d

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import rootReducers from "./redux/reducers";
 
 const initialState = {};
